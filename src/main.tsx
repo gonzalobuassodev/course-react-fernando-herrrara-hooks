778b6486ded2b00900f353109e051019c8a59c59
Gonzalo Buasso <gonzalobuasso@gmail.com>
2024-11-04 11:55:10 +0100

    Uso del use Ref

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithUseHook } from './02-useEffect/FormWithUseHook'
-import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
+// import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
